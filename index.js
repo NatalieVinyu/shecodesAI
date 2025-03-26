354925df11384d7b3e630fc4fdc9968d99ae9c81
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-03-26 14:59:04 +0200

    update

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ function displayPoem(response) {
     autostart: true,
     delay: 20
   })
-
 }
 
 function generateInput(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-poem")
+
   let apiKey = "04ot834cf4e3311e139ab83543b14df3"
   let context = "Make the poem short and simple"
   let prompt = "Generate a beautiful poem about love"
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "poem results here";
+  poemElement.innerHTML = `${searchInput}.value}`;
 
   console.log("Generating Poem...")
   axios.get(apiUrl).then(displayPoem)
