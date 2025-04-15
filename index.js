be7fe7d4accba9afc4d1cb9ce04c884c8e9b0485
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-04-15 16:55:58 +0200

    Implemented the user experience

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function displayPoem(response) {
-  console.log("poem generated")
   new Typewriter("#poem", {
     strings: response.data.answer,
-    autostart: true,
+    autoStart: true,
     delay: 1,
     cursor: "",
   });
@@ -13,9 +12,12 @@ function generateInput(event) {
 
   let searchInput = document.querySelector("#search-poem");
   let apiKey = "04ot834cf4e3311e139ab83543b14df3";
-  let context = "You are a romantic poem expert, make the poem short at least basic 5 lines and separate each line with <br />. Sign th e poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+  let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not include ```html Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem.";
   let prompt = `Generate a beautiful poem about ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
 
   console.log("Generating Poem...");
   console.log(`Prompt: ${prompt}`);
